bac3e8d789ae97dcec4af1166b3add761d9b3f57
Carolien Van Reeth <carolienvan_reeth@hotmail.com>
2026-05-12 14:50:47 +0200

    functies toegevoegd

--- Helper.py
+++ Helper.py
@@ -1,5 +1,5 @@
-tekst="header"
-lengte = len(tekst) + 4
-print(lengte * "*")
-print(f"* {tekst} *")
-print(lengte * "*")
+def decoreer(tekst=""):
+    lengte = len(tekst) + 4
+    print(lengte * "*")
+    print(f"* {tekst} *")
+    print(lengte * "*")
--- tijdelijk.py
+++ tijdelijk.py
@@ -1,13 +1,19 @@
-prijzen = {"Aardbei":3,
-           "Vanille":4,
-           "Chocolade":5}
-aanbieding = prijzen["Aardbei"]*0.8
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
-reclame_tekst2 = reclame_tekst[:62]
-reclame_tekst3 = reclame_tekst2.upper()
-reclame_tekst4 = reclame_tekst3.split()
-for el in reclame_tekst4:
-    if len(el) >=5:
-        print(el.upper())
-    else:
-        print(el.lower())
+from Helper import decoreer
+
+def print_aanbieding():
+    prijzen = {"Aardbei":3,
+            "Vanille":4,
+            "Chocolade":5}
+    aanbieding = prijzen["Aardbei"]*0.8
+    reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+    reclame_tekst2 = reclame_tekst[:63]
+    reclame_tekst3 = reclame_tekst2.upper()
+    reclame_tekst4 = reclame_tekst3.split()
+    for el in reclame_tekst4:
+        if len(el) >=5:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
