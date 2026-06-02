2dea5472555fb0424027181011442f12d0e4b66e
Carolien Van Reeth <carolienvan_reeth@hotmail.com>
2026-06-02 14:49:19 +0200

    Updates Les 10

--- Helper.py
+++ Helper.py
@@ -3,3 +3,22 @@ def decoreer(tekst=""):
     print(lengte * "*")
     print(f"* {tekst} *")
     print(lengte * "*")
+
+def fooi_pp(bedrag, personen):
+    try:
+        bedrag_pp = bedrag/personen
+    except:
+        bedrag_pp = "??"
+    return f"Het bedrag per persoon is {bedrag_pp} euro"
+
+def onderstreep(tekst=""):
+    uit = []
+    uit.append(tekst)
+    uit.append(len(tekst) * "=")
+    return uit
+
+def som(dictionary):
+    totaal = 0
+    for waarde in dictionary.values():
+        totaal += waarde
+    return totaal
---
+++ presentatie.py
@@ -0,0 +1,5 @@
+def presenteer(dictonary, totaal):
+    for key, value in dictonary.items():
+        print(f"{key} : {value} euro")
+    print("="*25)
+    print(f"totaal : {totaal} euro")
